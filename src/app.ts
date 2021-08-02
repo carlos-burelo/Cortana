e2f793144c087d6faa1eec5669f2e2c50d1be6a3
carlos-burelo <carlosfernandoburelo@gmai.com>
2021-08-02 15:56:10 -0500

    Corrigiendo variables de entorno

--- src/app.ts
+++ src/app.ts
@@ -7,41 +7,43 @@ import { mainConnect } from './database';
  * @return {boolean}
  */
 export function enviromentConfig() {
-  if (process.env['TOKEN'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['NODE_ENV'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['CURRENCY'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['CHANELID'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['CRYPTO'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['OWNERID'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['OWNERUSERNAME'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['OWNERNAME'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['BOTID'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['BOTUSERNAME'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['BOTNAME'] == undefined) {
-    throw new Error('Bot token is missing');
-  }
-  if (process.env['BOTREPO'] == undefined) {
-    throw new Error('Bot token is missing');
+  if(process.env.NODE_ENV == 'development'){
+    if (process.env['TOKEN'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['NODE_ENV'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['CURRENCY'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['CHANELID'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['CRYPTO'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['OWNERID'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['OWNERUSERNAME'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['OWNERNAME'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['BOTID'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['BOTUSERNAME'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['BOTNAME'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
+    if (process.env['BOTREPO'] == undefined) {
+      throw new Error('Bot token is missing');
+    }
   }
   if (existsSync(resolve(__dirname, 'downloads')) == false) {
     mkdirSync(resolve(__dirname, 'downloads'), { recursive: true });

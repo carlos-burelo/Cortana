a0fd7a5979ee2e8ad85c8ef907345a7761120140
carlos-burelo <carlosfernandoburelo@gmail.com>
2021-10-25 18:07:38 -0500

    restore imports method

--- src/core/modules/node/index.ts
+++ src/core/modules/node/index.ts
@@ -1,9 +1,7 @@
 import { Bot } from 'grammy';
 import { Cortana } from '../../../context';
+import { bashCmd } from './bash';
 
 export default function nodeModule(bot: Bot<Cortana>) {
-    bot.command('bash', async (ctx) => {
-        const { bashCmd } = await import('./bash');
-        return await bashCmd(ctx);
-    });
+  bot.command('bash', async (ctx) => await bashCmd(ctx));
 }
---
+++ src/core/modules/owner/index.ts
@@ -0,0 +1,7 @@
+import { Bot } from 'grammy';
+import { Cortana } from '../../../context';
+import { sendCmd } from './send';
+
+export default function ownerModule(bot: Bot<Cortana>) {
+  bot.command('send', async (ctx) => await sendCmd(ctx));
+}

225d0d534a82276150c47139b4b3626420e4fc33
carlos-burelo <carlosfernandoburelo@gmail.com>
2021-11-01 09:25:36 -0600

    remove sql comments

--- src/core/sql/bio.sql.ts
+++ src/core/sql/bio.sql.ts
@@ -1,57 +1,6 @@
 import { sql } from '.';
 import { BioI } from '@interfaces/sql';
 
-// export class BiosSQL {
-//   private chatId: number;
-//   private userId: number;
-//   constructor(ctx: Cortana) {
-//     this.chatId = ctx.chat.id;
-//     this.userId = ctx.msg.reply_to_message.from.id;
-//   }
-
-//   async getOneBio(): Promise<BiosTable | undefined> {
-//     const { data, error } = await sql
-//       .from<BiosTable>('bios')
-//       .select('content, type, format')
-//       .eq('id', this.chatId)
-//       .eq('userId', this.userId)
-//       .single();
-//     if (error) return undefined;
-//     else return data;
-//   }
-//   async addBio(bio: BiosTable): Promise<BiosTable | undefined> {
-//     const { data, error } = await sql
-//       .from<BiosTable>('bios')
-//       .insert(bio)
-//       .eq('id', this.chatId)
-//       .eq('userId', this.userId)
-//       .single();
-//     if (error) return undefined;
-//     else return data;
-//   }
-//   async removeBio(bio: BiosTable) {
-//     // unknow result
-//     const { data, error } = await sql
-//       .from<BiosTable>('bios')
-//       .delete()
-//       .eq('id', this.chatId)
-//       .eq('userId', bio.userId)
-//       .single();
-//     if (error) return undefined;
-//     else return data;
-//   }
-//   async updateBio(bio: BiosTable) {
-//     const { data, error } = await sql
-//       .from<BiosTable>('bios')
-//       .update(bio)
-//       .eq('id', this.chatId)
-//       .eq('userId', bio.userId)
-//       .single();
-//     if (error) return undefined;
-//     else return data;
-//   }
-// }
-
 export async function getBio(chatId: number, userId: number): Promise<BioI> {
   const { data, error } = await sql
     .from<BioI>('bios')

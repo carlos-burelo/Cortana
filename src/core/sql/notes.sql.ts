6ee41377bd95691200c1c77cd09c9bf223333408
carlos-burelo <carlosfernandoburelo@gmail.com>
2021-11-01 10:10:41 -0600

    documentation function in note

--- src/core/sql/notes.sql.ts
+++ src/core/sql/notes.sql.ts
@@ -1,6 +1,13 @@
 import { sql } from '.';
 import { NoteI } from '@interfaces/sql';
-
+/**
+ * SQL: notes table
+ * create one note in the notes table
+ * and return the status of the query
+ *
+ * @param  {NoteI} note
+ * @return {Promise<number>}
+ */
 export async function insertNote(note: NoteI): Promise<boolean> {
   const { error } = await sql.from<NoteI>('notes').insert(note);
   if (error) return false;

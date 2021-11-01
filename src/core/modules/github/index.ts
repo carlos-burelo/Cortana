bea7f1e0ce7aaf4e12a66984f97c284cc8a5dd58
carlos-burelo <carlosfernandoburelo@gmail.com>
2021-11-01 15:57:57 -0600

    fix command handler

--- src/core/modules/github/index.ts
+++ src/core/modules/github/index.ts
@@ -3,10 +3,11 @@ import { Cortana } from '@context';
 import { cloneCmd } from './clone';
 import { gitCmd } from './git';
 import { reposCmd } from './repos';
+import { repoCmd } from './repo';
 
 export default function githubModule(bot: Bot<Cortana>) {
   bot.command('git', async (ctx) => gitCmd(ctx));
-  bot.command('repo', async (ctx) => reposCmd(ctx));
+  bot.command('repo', async (ctx) => repoCmd(ctx));
   bot.command('repos', async (ctx) => reposCmd(ctx));
   bot.command('clone', async (ctx) => cloneCmd(ctx));
 }

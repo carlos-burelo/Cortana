ebe3e447bcb1cfcc3e56c7fc3c5050a3bcca0599
carlos-burelo <carlosfernandoburelo@gmail.com>
2021-11-13 17:30:40 -0600

    removing repoScrapping funtion by unnecesary

--- src/core/libs/scraping.ts
+++ src/core/libs/scraping.ts
@@ -13,49 +13,6 @@ export interface RepoInterface {
   forks: number;
   stars: number;
 }
-// export interface RepoInterfaceResponse {
-
-// }
-
-// export async function scrapingRepo(user:string,repo:string ): Promise<RepoInterface> {
-//   const {data} = await axios.get(`https://api.github.com/repos/${user}/${repo}`);
-//   // const $ = parse(data);
-
-//   return
-// }
-
-export async function repoScrapping(
-  user: string,
-  repo: string
-): Promise<RepoInterface | undefined> {
-  try {
-    const url = `https://github.com/${user}/${repo}`;
-    const { data }: AxiosResponse = await axios.get(url);
-    const $ = Cheerio.load(data);
-    const description: string = $('p.f4.mt-3').text().trim();
-    const branch: string = $('summary span.css-truncate-target').text();
-    const link: string = `https://github.com/${user}/${repo}`;
-    const download: string = `https://github.com/${user}/${repo}/archive/refs/heads/${branch}.zip`;
-    const stars = parseInt(
-      $('.pagehead-actions.flex-shrink-0.d-none.d-md-inline li:nth-child(2)')
-        .text()
-        .replace(/\s|\D/g, '')
-    );
-    const forks = parseInt(
-      $('.pagehead-actions.flex-shrink-0.d-none.d-md-inline li:nth-child(3)')
-        .text()
-        .replace(/\s|\D/g, '')
-    );
-    const tags: any[] = $('a.topic-tag.topic-tag-link')
-      .map((i, e) => {
-        return $(e).text().trim();
-      })
-      .toArray();
-    return { link, description, branch, download, tags, forks, stars };
-  } catch (error) {
-    return undefined;
-  }
-}
 
 export async function fwScrapping(csc: string, model: string) {
   try {

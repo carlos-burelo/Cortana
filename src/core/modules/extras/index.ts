108c282313396ca49dbe1bce3d469d3fb38a8632
carlos-burelo <carlosfernandoburelo@gmail.com>
2021-11-14 12:04:16 -0600

    remove highlight command, remove unnecessary dependency and update ts-node

--- src/core/modules/extras/index.ts
+++ src/core/modules/extras/index.ts
@@ -2,7 +2,6 @@ import { Bot } from 'grammy';
 import { Cortana } from '@context';
 import { mdv2Cmd } from './mdv2';
 import { mdv2helpCmd } from './mdv2help';
-// import { highlightCmd } from './highlight';
 import { loliCmd } from './loli';
 import { ccCmd } from './currency';
 import { commandsCmd } from './commands';
@@ -10,7 +9,6 @@ import { commandsCmd } from './commands';
 export default function extrasModule(bot: Bot<Cortana>) {
   bot.command('mdv2', async (ctx) => await mdv2Cmd(ctx));
   bot.command('mdv2help', async (ctx) => await mdv2helpCmd(ctx));
-  // bot.command('hl', async (ctx) => await highlightCmd(ctx));
   bot.command('loli', async (ctx) => await loliCmd(ctx));
   bot.command('cc', async (ctx) => await ccCmd(ctx));
   bot.command('cmds', async (ctx) => await commandsCmd(ctx));
